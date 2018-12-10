aa5a46ed9dd106bc84606d834e1811ca9c622aca
keven <imbeault.keven@gmail.com>
2018-12-10 14:32:00 -0500

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {page:'Accueil', menuId:'home'});
+});
+
+router.get('/logiciels', function(req, res, next) {
+  res.render('index', {page:'Logiciels', menuId:'logiciels'});
+});
+
+router.get('/competences', function(req, res, next) {
+  res.render('index', {page:'Compétences', menuId:'competences'});
+});
+
+module.exports = router;
